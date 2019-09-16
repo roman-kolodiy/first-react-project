5cebe5ac4a2bbf9f5337a326124e54ef6b494ee2
Pater Perker <kitakun3@gmail.com>
2019-09-16 12:53:27 +0300

    Add LOADER component/state/action (loader.svg)

---
+++ src/components/Common/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import preloaderSVG from '../../assets/images/loader.svg';
+
+
+const Preloader = () => {
+    return <>
+        <img src={preloaderSVG} alt='missing'/>
+    </>
+};
+
+export default Preloader;
